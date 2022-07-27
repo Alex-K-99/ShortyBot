aeea9d8bcc3094d896d44eb4b47f74d8d654badc
Alex <alkn@gmx.net>
2022-07-27 14:02:07 +0200

    Changed Process Path of .bat to correct path

--- src/main/java/com/lexn/listener/CommandListener.java
+++ src/main/java/com/lexn/listener/CommandListener.java
@@ -33,7 +33,7 @@ public class CommandListener extends ListenerAdapter {
                 if(args[0].equalsIgnoreCase(prefix+"boot")){
                     messageObject.reply("Booting Server. (meme)").queue();
                     try {
-                        Process p = new ProcessBuilder("C:\\Users\\Minecraft Server\\Desktop\\mc server\\paper.jar").start();
+                        Process p = new ProcessBuilder("C:\\Users\\Minecraft Server\\Desktop\\mc server\\start_paper.bat").start();
                         p.waitFor();
                     } catch (IOException | InterruptedException e) {
                         throw new RuntimeException(e);
