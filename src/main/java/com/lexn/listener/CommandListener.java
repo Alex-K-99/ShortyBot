ed8b0852074f0e7c84288e0d628b8ddb3f2a7f08
Alex <alkn@gmx.net>
2022-07-27 16:35:59 +0200

    added absolute patch to server jar

--- src/main/java/com/lexn/listener/CommandListener.java
+++ src/main/java/com/lexn/listener/CommandListener.java
@@ -34,7 +34,7 @@ public class CommandListener extends ListenerAdapter {
                     messageObject.reply("Booting Server. (meme)").queue();
                     try {
                         System.out.println("Trying to boot server...");
-                        Process p = new ProcessBuilder("java -Xms20G -Xmx20G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs/ -Daikars.new.flags=true -jar C:\\Users\\Minecraft Server\\Desktop\\mc server\\paper.jar --nogui").start();
+                        Process p = new ProcessBuilder("cmd.exe").start();
                         p.waitFor();
                     } catch (IOException | InterruptedException e) {
                         throw new RuntimeException(e);
